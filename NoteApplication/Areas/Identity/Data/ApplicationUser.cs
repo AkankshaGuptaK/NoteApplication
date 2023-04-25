8cac8a195a1cf88c923894e1cc8ffbdee2289e73
Akanksha Gupta <akanksha.g@xitsolutionsinc.com>
2023-04-25 08:22:25 +0530

    Add project files.

---
+++ NoteApplication/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NoteApplication.Areas.Identity.Data
+{
+    public class ApplicationUser: IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ NoteApplication/Migrations/20230424215338_DBUpdate1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NoteApplication.Migrations
+{
+    public partial class DBUpdate1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
